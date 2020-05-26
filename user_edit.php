d8ff3997637f9d91a77719b79007374b49811385
root <root@simpnas.local>
2020-05-26 13:57:39 -0400

    fixed user_edit missing }

--- user_edit.php
+++ user_edit.php
@@ -1,24 +1,24 @@
 <?php 
-    $config = include("config.php");
-  	include("simple_vars.php");
-    include("header.php");
-    include("side_nav.php");
-    if(isset($_GET['username'])){
-  		$username = $_GET['username'];
-  	}
-    $primary_group = exec("id -g -n $username");
-    exec("awk -F: '$3 > 999 {print $1}' /etc/group | grep -v nogroup", $group_array);
-    $group_member_array = explode(' ',exec("groups $username"));
+  $config = include("config.php");
+	include("simple_vars.php");
+  include("header.php");
+  include("side_nav.php");
+  if(isset($_GET['username'])){
+		$username = $_GET['username'];
+	}
+  $primary_group = exec("id -g -n $username");
+  exec("awk -F: '$3 > 999 {print $1}' /etc/group | grep -v nogroup", $group_array);
+  $group_member_array = explode(' ',exec("groups $username"));
 ?>
 
 <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
   <nav>
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-    <li class="breadcrumb-item"><a href="users.php">Users</a></li>
-    <li class="breadcrumb-item active">Edit User</li>
-  </ol>
-</nav>
+	  <ol class="breadcrumb">
+	    <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
+	    <li class="breadcrumb-item"><a href="users.php">Users</a></li>
+	    <li class="breadcrumb-item active">Edit User</li>
+	  </ol>
+	</nav>
 
   <h2>Edit User</h2>
   <form method="post" action="post.php">
@@ -40,14 +40,16 @@
 	    <label class="form-check-label ml-1">users</label>
 	  </div>
 	  
-	  <?php foreach ($group_array as $group) { ?>
+	  <?php 
+	  foreach($group_array as $group){ 
+	  ?>
 	  		
-	  <div class="form-group form-check">
-	    <input type="checkbox" class="form-check-input" name="group[]" <?php foreach ($group_member_array as $group_member) {if($group == $group_member){ echo "checked"; } ?> value="<?php echo "$group"; ?>">
-	    <label class="form-check-label ml-1"><?php echo "$group"; ?></label>
-	  </div>
-
-	  <?php } ?>
+		  <div class="form-group form-check">
+		    <input type="checkbox" class="form-check-input" name="group[]" <?php foreach($group_member_array as $group_member){ if($group == $group_member){ echo "checked"; } ?> value="<?php echo $group; ?>" <?php } ?>>
+		    <label class="form-check-label ml-1"><?php echo "$group"; ?></label>
+		  </div>
+		  
+		<?php } ?>
 
 	  <button type="submit" name="user_edit" class="btn btn-primary">Submit</button>
    </form>
