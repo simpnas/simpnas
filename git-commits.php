fa6b323416e33f4767e6aa7a7b7204853ecbe57f
root <root@simpnas.local>
2020-06-01 19:27:57 -0400

    show current commit on git-commit.php

--- git-commits.php
+++ git-commits.php
@@ -3,6 +3,7 @@
   include("simple_vars.php");
   include("header.php");
   include("side_nav.php");
+  $current_version = exec("git rev-parse --short HEAD");
   $git_log = shell_exec("git log --pretty=format:'<tr><td>%h</td><td>%an</td><td>%ar</td><td>%s</td></tr>'");
 ?>
 
@@ -10,8 +11,7 @@
 
    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-2">
     <h2>Updates</h2>
-    <a href="updates.php?check" class="btn btn-outline-primary">Check For OS Updates</a>
-    <a href="post.php?upgrade" class="btn btn-outline-secondary">Upgrade OS Packages</a>
+    Current Version: <?php echo $current_version; ?>
     <a href="post.php?upgrade_simpnas_overwrite_local_changes" class="btn btn-outline-secondary">Upgrade SimpNAS</a>
   </div>
   <table class="table ">
