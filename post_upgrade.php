62d2fd0bbdb183a21191432bf9500c5a3d9c3dfa
root <root@simpnas.local>
2020-07-03 19:39:42 -0400

    Added a check to post_upgrade to see if btrfs is installed before installing

--- post_upgrade.php
+++ post_upgrade.php
@@ -3,6 +3,9 @@
 //Post Upgrade Tasks
 
 //Install BTRFS to enable support for the filesystem
-exec("apt install btrfs-progs -y");
+$btrfs_installed = exec("apt list --installed btrfs-progs | grep installed");
+if(empty($btrf_installed)){
+	exec("apt install btrfs-progs -y");
+}
 
 ?>
