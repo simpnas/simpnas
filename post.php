c2d74ddae4207cb2444c08e3e77f9126bc6c7757
root <root@simpnas.local>
2020-07-19 19:05:54 -0400

    Wipe out any superblocks in partitions before creating an array in initial setup

--- post.php
+++ post.php
@@ -1962,6 +1962,9 @@ if(isset($_POST['setup_volume_raid'])){
   }
 
   $diskparts = implode(' ',$diskpart_array);
+
+  //WIPE out any superblocks
+  exec("mdadm --zero-superblock $diskparts");
   
   //Generate the next /dev/mdX Number
   //get the last md#
