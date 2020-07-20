a90832de8714d5c09fbe7960f29f2df555b4d425
root <root@simpnas.local>
2020-07-20 13:03:07 -0400

    Updated Configure Transmission VPN UI

--- configure_transmission.php
+++ configure_transmission.php
@@ -21,49 +21,38 @@
   </ol>
 </nav>
 
-  <h2>Configure Transmission</h2>
+  <h2>Configure Transmission VPN</h2>
   <h4>VPN IP: <small><?php $vpn_ip = exec("docker exec transmission curl ifconfig.co"); echo $vpn_ip; ?></small></h4>
  
   <form method="post" action="post.php">
-
+		  
 	  <div class="form-group">
-	  	<div class="custom-control custom-checkbox">
-			  <input type="checkbox" class="custom-control-input" name="enable_vpn" value="1" id="configVpn" <?php if(!empty($vpn_user)) { echo "checked"; } ?>>
-			  <label class="custom-control-label" for="configVpn">Configure VPN</label>
-			</div>
+	    <label>VPN Provider <strong class="text-danger">*</strong></label>
+	    <select class="form-control" name="vpn_provider">
+	  		<option>PIA</option>
+	  	</select>
+	  	<small class="form-text text-muted">We only support PIA VPN for right now.</small>
 	  </div>
 	  
-	  <div id="vpnSettings">		  
-		  
-		  <div class="form-group">
-		    <label>VPN Provider <strong class="text-danger">*</strong></label>
-		    <select class="form-control" name="vpn_provider">
-		  		<option>PIA</option>
-		  	</select>
-		  	<small class="form-text text-muted">We only support PIA VPN for right now.</small>
-		  </div>
-		  
-		  <div class="form-group">
-		    <label>VPN Region / Server <small class="text-secondary">(Optional)</small></label>
-		    <input type="text" class="form-control" name="vpn_server" placeholder="Optional (Random by Default)">
-		  </div>
-		  
-		  <div class="form-group">
-		    <label>VPN Username <strong class="text-danger">*</strong></label></label>
-		    <input type="text" class="form-control" name="username" placeholder="Username" value="<?php echo $vpn_user; ?>">
-		  </div>
-		  
-		  <div class="form-group">
-		    <label>VPN Password <strong class="text-danger">*</strong></label></label>
-		    <input type="password" class="form-control" name="password" placeholder="Password" value="<?php echo $vpn_pass; ?>">
-		  </div>
-		  
-		  <div class="form-group">
-		    <label>DNS Server <small class="text-secondary">(Optional)</small></label>
-		    <input type="text" class="form-control" name="dns" placeholder="1.1.1.1">
-		  	<small class="form-text text-muted">Required if your current dns server isn't accessable outside its own network (If you have Comcast then user a public DNS) Common DNS Servers 8.8.8.8 (Google) 1.1.1.1 (Cloudflare) 9.9.9.9 (Quad9)</small>
-		  </div>
+	  <div class="form-group">
+	    <label>VPN Region / Server <small class="text-secondary">(Optional)</small></label>
+	    <input type="text" class="form-control" name="vpn_server" placeholder="Optional (Random by Default)">
+	  </div>
 	  
+	  <div class="form-group">
+	    <label>VPN Username <strong class="text-danger">*</strong></label></label>
+	    <input type="text" class="form-control" name="username" placeholder="Username" value="<?php echo $vpn_user; ?>">
+	  </div>
+	  
+	  <div class="form-group">
+	    <label>VPN Password <strong class="text-danger">*</strong></label></label>
+	    <input type="password" class="form-control" name="password" placeholder="Password" value="<?php echo $vpn_pass; ?>">
+	  </div>
+	  
+	  <div class="form-group">
+	    <label>DNS Server <small class="text-secondary">(Optional)</small></label>
+	    <input type="text" class="form-control" name="dns" placeholder="1.1.1.1">
+	  	<small class="form-text text-muted">Required if your current dns server isn't accessable outside its own network (If you have Comcast then user a public DNS) Common DNS Servers 8.8.8.8 (Google) 1.1.1.1 (Cloudflare) 9.9.9.9 (Quad9)</small>
 	  </div>
  		
  		<button type="submit" name="transmission_update" class="btn btn-primary">Submit</button>
