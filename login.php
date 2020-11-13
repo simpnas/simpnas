bd6dea4dac0d17147bc58c47fe50113bdb4c5aac
root <johnny@pittpc.com>
2020-11-12 21:42:22 -0500

    Added File Manager button on login

--- login.php
+++ login.php
@@ -58,6 +58,8 @@ if(isset($_POST['login'])){
       <input type="text" id="inputUsername" name="username" class="form-control" placeholder="Username" required autofocus>
       <input type="password" id="inputPassword" class="form-control" name="password" placeholder="Password" required>
       <button type="submit" class="btn btn-primary p-2 btn-block" name="login">Login</button>
+      <a href="http://<?php echo $config_primary_ip; ?>:82" class="btn btn-secondary p-2 btn-block">File Manager</a>
+    </form>
     </form>
   
 <?php include("footer.php"); ?>
